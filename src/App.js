13e263cd46facbf28c4ad44ea7f01fd07a111c82
chickenmilkcream <zhiting.peng@mail.utoronto.ca>
2022-12-24 20:44:59 -0400

    goodbye amogus

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import Timeline from "./components/timeline";
-import amongUs from "./amongUs.json";
+import amongUs from "./json/amongUs.json";
+import stars from "./json/stars.json";
+import snow from "./json/snow.json";
+import nyanCat from "./json/nyanCat.json";
 
 export default function App() {
   const particlesInit = async (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
     await loadFull(main);
   };
 
@@ -17,7 +15,7 @@ export default function App() {
     <div className="App">
       <h1>Hello Coders!</h1>
 
-      <Particles id="tsparticles" init={particlesInit} options={amongUs} />
+      <Particles id="tsparticles" init={particlesInit} options={snow} />
       <Timeline></Timeline>
     </div>
   );
