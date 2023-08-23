984ed2cac63bfd7de8b20acf5cb6c8d2b9d96702
chickenmilkcream <zhiting.peng@mail.utoronto.ca>
2023-08-23 18:17:44 -0400

    small fixes

--- src/containers/education/Education.jsx
+++ src/containers/education/Education.jsx
@@ -4,20 +4,13 @@ const Education = () => {
   return (
     <div id="education">
       <h2>Education</h2>
-      <h3>
-        <a
-          href="https://www.utoronto.ca/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          University of Toronto
-        </a>
-      </h3>
+      <h3>University of Toronto</h3>
       <h4>Honours BSc, Computer Science + Statistics Major, Math Minor</h4>
       <h5>09.2018 - 06.20 </h5>
       <p>
         My time here was a real rollercoaster. Half of it was virtual thanks to
-        COVID, which meant lots of Discord study sessions. But it challenged me academically and I could always admire the{" "}
+        COVID, which meant lots of Discord study sessions. But it challenged me
+        academically and I could always admire the{" "}
         <a
           href="https://en.wikipedia.org/wiki/Robarts_Library"
           target="_blank"
@@ -25,8 +18,8 @@ const Education = () => {
         >
           brutalist turkey library
         </a>{" "}
-        whenever I get too stressed. And the best part? I made some awesome friends
-        who kept me sane through it all. Can't believe it's over! 🎓😄
+        whenever I get too stressed. And the best part? I made some awesome
+        friends who kept me sane through it all. Can't believe it's over! 🎓😄
       </p>
     </div>
   );
